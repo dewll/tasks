fdcfcd8e740accd5e321e77150f8e92273c3a37f
Balogun Sodiq <63922629+dewll@users.noreply.github.com>
2022-04-13 17:33:37 +0100

    rename

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,5 +5,5 @@ import * as functions from "firebase-functions";
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Balogun!");
+  response.send("Hello from Balogun Sodiq!");
 });
