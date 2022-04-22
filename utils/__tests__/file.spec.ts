8dec2c009e11da49e5277f6c676962aa03e93b2e
dwell <sodiqb86@gmial.com>
2022-04-22 13:55:06 -0700

    commit

--- utils/__tests__/file.spec.ts
+++ utils/__tests__/file.spec.ts
@@ -9,7 +9,7 @@ describe('FileUtils', ()=>{
     const createTempFilePath = FileUtils.createTempFilePath(fileName,folderPath)
     expect(createTempFilePath).toBeTruthy();
   });
-  it('testing if  a new text document is created for writeStream in the temp file directory', () => {
+  it('testing if  a new text document is created for writeStream in the temp file directory.', () => {
     const createTempFile = FileUtils.createTempFile(`${FileUtils.tmpFolder()}/${folderPath}/${newDocument}`)
     expect(createTempFile).toBeTruthy();
   });
