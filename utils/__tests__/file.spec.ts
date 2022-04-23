36be4da802d3a4841fabb4b3e17898f1711ab431
Balogun Sodiq <63922629+dewll@users.noreply.github.com>
2022-04-23 12:17:45 +0100

    another refactoring

--- utils/__tests__/file.spec.ts
+++ utils/__tests__/file.spec.ts
@@ -1,29 +1,24 @@
 import {FileUtils} from '../file';
 
-const fileName = 'content.txt';
 const newDocument = 'New Text Document.txt'
 const folderPath = 'testFolder';
 
-describe('FileUtils', ()=>{
+describe('FileUtils', () => {
   it('testing if  a new text document is created for writeStream in the temp file directory.', () => {
-    const createTempFilePath = FileUtils.createTempFilePath(fileName,folderPath)
-    const createTempFile = FileUtils.createTempFile(`${FileUtils.tmpFolder}/${folderPath}/${newDocument}`)
-    expect(createTempFile).toBeTruthy();
-  });
-  it(' testing if a file is downloaded and written to  New Text Document', () => {
-    const downloadTempFile = FileUtils.downloadTempFile('https://www.google.com/', `${FileUtils.tmpFolder}/${folderPath}/${fileName}`)
-    const getFileName = FileUtils.getFileName(`${FileUtils.tmpFolder}/${folderPath}/${newDocument}`)
-    const removeTempFile = FileUtils.removeTempFile(`${FileUtils.tmpFolder}/${folderPath}/${newDocument}`)
-    expect(downloadTempFile).toBeTruthy();
+    const createTempFilePath = FileUtils.createTempFilePath(newDocument,folderPath)
+    const createTempFile = FileUtils.createTempFile(createTempFilePath)
+    const downloadTempFile = FileUtils.downloadTempFile('https://www.google.com/', createTempFilePath)
+    const getFileName = FileUtils.getFileName(createTempFilePath)
+    const removeTempFile = FileUtils.removeTempFile(createTempFilePath)
+    const getFolderPath = FileUtils.getFolderPath(createTempFilePath)
+    expect(createTempFile).toBeTruthy;
   });
   it('testing if a temp folder is created if not create new one', () => {
     const ensureFolder = FileUtils.createTempFolder(`${FileUtils.tmpFolder}/${folderPath}`)
-    const getFolderPath = FileUtils.getFolderPath(`${FileUtils.tmpFolder}/${folderPath}/${fileName}`)
-    expect(getFolderPath).toBe(`${FileUtils.tmpFolder}/${folderPath}`);
+    expect(ensureFolder).toBe(undefined);
   });
-
   it('testing if the folderpath exit, if it exit remove the file inside it and if it does not exit do notting', () => {
     const removeDir =  FileUtils.removeDir(`${FileUtils.tmpFolder}/${folderPath}`)
-    expect(removeDir).toBeTruthy();
+    expect(removeDir).toBeTruthy;
   });
 })
