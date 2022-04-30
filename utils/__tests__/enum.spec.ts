a59cb47e53e56b640ed3847d72fa6823b216a3ab
dwell <sodiqb86@gmial.com>
2022-04-30 18:46:10 +0100

    enum commit

--- utils/__tests__/enum.spec.ts
+++ utils/__tests__/enum.spec.ts
@@ -6,7 +6,9 @@ enum myEnum{
   Left,
   Right,
 }
+
 const enumValue = 1
+
 describe(' EnumUtils ', () => {
 it('testing if length of enum is greater than 0 and return the value but retun 0 if length is less than zero ', () => {
     const getEnumKeyByEnumValue =EnumUtils.getEnumKeyByEnumValue(myEnum, enumValue)
