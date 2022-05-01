cc48c7687c82f8c21008493fdd547cc6608da617
dwell <sodiqb86@gmial.com>
2022-05-02 00:17:13 +0100

    initial commit

--- utils/__tests__/exceptions.spec.ts
+++ utils/__tests__/exceptions.spec.ts
@@ -1,16 +1,19 @@
 import { ExceptionUtils } from '../exception';
 
+const error = 'HELLO';
+const errorCode = 461;
+
 describe('ExceptionUtils', () => {
 it('testing if error was thrown.', () => {
   expect.assertions(0);
   try{
-    expect(ExceptionUtils.throwError('good')).toThrow('some error');
+    expect(ExceptionUtils.throwError(error)).toThrow(error);
   } catch (e) {}
 });
 it('testing if validationerror was thrown.', () => {
   expect.assertions(0);
   try{
-    expect(ExceptionUtils.throwValidationError('good',461)).toThrow('some error');
+    expect(ExceptionUtils.throwValidationError(error, errorCode)).toThrow(error);
   } catch (e) {}
 });
 })
